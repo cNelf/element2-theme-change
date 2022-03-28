f367825be012e95bf66eb3f69dbc2bd7dafa16b3
CNelfHope <5131361+CNelfHope@user.noreply.gitee.com>
2022-03-28 21:40:19 +0800

    feat: 实现主题色切换效果

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="text">主题色本文</div>
+    <el-button type="primary">主题色按钮</el-button>
+    <div>
+      <span>修改主题色：</span>
+      <el-color-picker v-model="themeColor" @change="handleChangeTheme" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import { changeThemeColor } from './utils/color'
+
+  export default {
+    name: 'App',
+
+    data() {
+      return {
+        themeColor: '#409EFF'
+      }
+    },
+
+    methods: {
+      handleChangeTheme(color) {
+        changeThemeColor(color)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
